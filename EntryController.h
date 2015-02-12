5f2f4481dd0d1a81d645f5e6fd06e0132eeed0b3
christianmonson <christianrmonson@gmail.com>
2015-02-11 17:00:28 -0700

    completes step 18
    creates an EntryController Object

---
+++ EntryController.h
@@ -0,0 +1,22 @@
+//
+//  EntryController.h
+//  DayX
+//
+//  Created by Christian Monson on 2/11/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Entry.h"
+
+@interface EntryController : NSObject
+
+@property (nonatomic,strong,readonly) NSArray *entries;
+
++ (EntryController *)sharedInstance;
+
+- (void)addEntry:(Entry *)entry;
+- (void)removeEntry:(Entry *)entry;
+- (void)replaceEntry:(NSDictionary *)oldEntry withEntry:(NSDictionary *)newEntry;
+
+@end
---
+++ EntryController.m
@@ -0,0 +1,75 @@
+//
+//  EntryController.m
+//  DayX
+//
+//  Created by Christian Monson on 2/11/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "EntryController.h"
+
+@interface EntryController ()
+
+@property (nonatomic,strong) NSArray *entries;
+
+@end
+
+@implementation EntryController
+
++ (EntryController *)sharedInstance {
+    static EntryController *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[EntryController alloc] init];
+        
+        [sharedInstance loadFromDefaults];
+    });
+    return sharedInstance;
+}
+
+- (void)addEntry:(Entry *)entry{
+    
+    if (!entry) {
+        return;
+    }
+    
+    NSMutableArray *mutableEntries = [[NSMutableArray alloc]initWithArray:self.entries];
+    [mutableEntries addObject:entry];
+    
+    self.entries = mutableEntries;
+//    [self synchronize];
+
+}
+
+- (void)removeEntry:(Entry *)entry{
+    
+    if (!entry) {
+        return;
+    }
+    
+    NSMutableArray *mutableEntries = self.entries.mutableCopy;
+    [mutableEntries removeObject:entry];
+    
+    self.entries = mutableEntries;
+//    [self synchronize];
+
+}
+
+- (void)replaceEntry:(NSDictionary *)oldEntry withEntry:(NSDictionary *)newEntry{
+    
+    if (!oldEntry || !newEntry) {
+        return;
+    }
+    
+    NSMutableArray *mutableEntries = self.entries.mutableCopy;
+    
+    if ([mutableEntries containsObject:oldEntry]) {
+        NSInteger index = [mutableEntries indexOfObject:oldEntry];
+        [mutableEntries replaceObjectAtIndex:index withObject:newEntry];
+    }
+    
+    self.entries = mutableEntries;
+//    [self synchronize];
+}
+
+@end
